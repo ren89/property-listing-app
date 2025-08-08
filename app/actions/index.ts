666b9be0efd8bb620ba1f4d62f31193135f7d9eb
ren89 <ren.avellano@gmail.com>
2025-08-08 21:03:38 +0800

    create user upon sign up

---
+++ app/actions/index.ts
@@ -0,0 +1,5 @@
+// User operations
+export { getUserById, createUser, getCurrentUser } from "./users";
+
+// Auth operations (re-export from main actions file)
+export { login, signup, logout } from "./actions";
