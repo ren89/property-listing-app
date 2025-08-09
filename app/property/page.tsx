d92960e0614691a167c8f6ded9ed42ba0f7ae351
ren89 <ren.avellano@gmail.com>
2025-08-09 13:18:19 +0800

    use role from jwt to restrict routes

---
+++ app/property/page.tsx
@@ -0,0 +1,8 @@
+export default function PropertyPage() {
+  return (
+    <div>
+      <h1>Property Page</h1>
+      {/* Additional content for the property page can be added here */}
+    </div>
+  );
+}
